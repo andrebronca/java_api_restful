ba89d0092faf2d2f03f0e9292115653d83d9bf0f
André R.B. (mac) <bronca.andre@gmail.com>
2022-08-10 17:06:25 -0300

    configuracao no mac

--- primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/services/ProdutoService.java
+++ primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/services/ProdutoService.java
@@ -16,28 +16,33 @@ public class ProdutoService {
     private ProdutoRepository produtoRepository;
 
     public List<Produto> obterTodos(){
-        return produtoRepository.obterTodos();
+        // return produtoRepository.obterTodos();
+        return produtoRepository.findAll();
     }
 
     public Optional<Produto> obterPorId(Integer id){
         //poderia verificar se o id é válido
-        return produtoRepository.obterPorId(id);
+        //return produtoRepository.obterPorId(id);
+        return produtoRepository.findById(id);
     }
 
     public Produto adicionar(Produto produto){
         //poderia ter uma regra pra validar o produto com determinada regra
         //validar qtd, nome não nulo, nr não negativo, etc
-        return produtoRepository.adicionar(produto, true);
+        //return produtoRepository.adicionar(produto, true);
+        return produtoRepository.save(produto);
     }
 
     public void deletar(Integer id){
-        produtoRepository.deletar(id);
+        // produtoRepository.deletar(id);
+        produtoRepository.deleteById(id);
     }
 
     public Produto atualizar(Integer id, Produto produto){
         if (id > 0){
             produto.setId(id);
-            return produtoRepository.atualizar(produto);
+            // return produtoRepository.atualizar(produto);
+            return produtoRepository.save(produto);
         }
         return null;
     }
