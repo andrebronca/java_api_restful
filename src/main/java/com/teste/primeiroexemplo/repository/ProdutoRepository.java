edab53cd1c65a718c94cb6aa004609403183f0b0
André R.B. (mac) <bronca.andre@gmail.com>
2022-08-12 18:05:27 -0300

    Reconfiguração do projeto para funcionar

--- src/main/java/com/teste/primeiroexemplo/repository/ProdutoRepository.java
+++ src/main/java/com/teste/primeiroexemplo/repository/ProdutoRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.teste.primeiroexemplo.model.Produto;
-import com.teste.primeiroexemplo.shared.ProdutoDTO;
-
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
     
